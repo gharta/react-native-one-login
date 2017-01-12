8967a263a24ca35a4516c5cd9884f374000e4f28
zainul-realtime <zainul@realtimejakarta.com>
2017-01-12 09:07:47 +0700

    adding ios bundle

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,80 @@
 /**
- * Sample React Native App
+ * React Native App
  * https://github.com/facebook/react-native
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+ import React from 'react';
+ import { AppRegistry, BackAndroid, AsyncStorage, Image} from 'react-native';
+ import { Scene, Router, Reducer, Actions, ActionConst } from 'react-native-router-flux';
+ import LoginPage from './app/components/Login';
+ import ListsExternalPage from './app/components/ListsExternalPage';
+ import WebViewPage from './app/components/Webview';
+ import styles from './app/styles/App';
 
-export default class realtimeLogin extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+ export default class realtimeLogin extends React.Component {
+   constructor () {
+     super()
+
+     this.state = {
+       isDefaultHome: false,
+       isDefaultLogin: false,
+       isGetFromStorage:false
+     }
+
+     this.preInitialize().done();
+   }
+
+   componentWillMount() {
+   };
+
+   async preInitialize() {
+
+    const profileJSON = await AsyncStorage.getItem('token');
+    const profile = JSON.parse(profileJSON);
+
+    if (profile != null) {
+      this.setState({
+        isDefaultHome: true
+      });
+    }else {
+      this.setState({
+        isDefaultLogin: true
+      });
+    }
+
+    this.setState({isGetFromStorage:true})
+
+    return true;
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+   render() {
+
+    if (!this.state.isGetFromStorage) {
+      return <Image source={ require('./background.jpg') } style={styles.container}></Image>;
+    }
+
+    return (
+      <Router createReducer={(params)=>{
+         const defaultReducer = Reducer(params);
+         return (state, action)=>{
+             if (action.key) {
+               this.setState({
+                 currentPage:action.key
+               })
+             }
+             return defaultReducer(state, action);
+         }
+       }}>
+        <Scene key="root">
+          <Scene key="ListsExternalPage" initial={this.state.isDefaultHome} type={ActionConst.REPLACE} hideNavBar={true} component={ListsExternalPage} />
+          <Scene key="LoginPage" initial={this.state.isDefaultLogin} type={ActionConst.REPLACE} hideNavBar={true} component={LoginPage} />
+          <Scene key="WebViewPage" hideNavBar={true} component={WebViewPage} />
+        </Scene>
+      </Router>
+     );
+
+   }
+ }
 
 AppRegistry.registerComponent('realtimeLogin', () => realtimeLogin);
